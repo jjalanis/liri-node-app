8d5886b09439c65d4354f4868bcec081e6240259
jjalanis <jjalanis@gmail.com>
2018-06-17 19:02:24 -0500

    movie and spotify

--- liri.js
+++ liri.js
@@ -1,20 +1,132 @@
+
 require("dotenv").config();
 var keys = require('./keys');
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
+var song = process.argv[3];
+
+var tweets = function()
+{
+    console.log("entro a twitter");
+    var Twitter = require('twitter');
+    
+    var client = new Twitter({
+    consumer_key: keys.twitter.consumer_key,
+    consumer_secret: keys.twitter.consumer_secret,
+    access_token_key: keys.twitter.access_token_key,
+    access_token_secret: keys.twitter.access_token_secret
+    });
+
+    var params = {
+        count: 20
+    };
+    client.get('statuses/home_timeline', params, function(error, tweets, response) {
+    if (!error) {
+    // console.log(tweets[0]);
+        for (var i=0;i<tweets.length;i++){
+        console.log("Tweet No."+(i+1)+": "+tweets[i].text+" created at: "+tweets[i].created_at);
+        }}
+    else{
+        console.log(error);
+        };
+    });
+}
+
+var spotifythis = function (){
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify({
+      id: keys.spotify.id,
+      secret: keys.spotify.secret
+    });
+    // if no argument given, then search default
+    if (song === undefined){
+         song = "The Sign by Ace of Base"
+    }
+
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+     var items = data.tracks.items[0];
+     var artist = items.artists[0];
+
+    //console.log(items);
+    console.log("========= SONG THIS =========");
+    console.log("Song: "+ items.name);
+    console.log("Artista: " + artist.name);
+    console.log("Preview Link: " + items.external_urls.spotify);
+    console.log("Album: " + items.album.name);
+    console.log("========= SONG THIS =========");
+     
+    });
+}
+
+var movie = function (){
+    var request = require("request");
+    var nodeArgs = process.argv;
+    var temp=0;
+
+    if (process.argv[3] === undefined){
+        var movieName = "Mr. Nobody"
+   }
+   else{
+    var movieName = "";
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
+            }
+            else {
+                movieName += nodeArgs[i];
+        }
+        }
+   }
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    //console.log(queryUrl);
+    request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        if (JSON.parse(body).Title !== undefined){
+        console.log("========= MOVIE THIS =========")
+        console.log("TITLE: "+JSON.parse(body).Title)
+        console.log("YEAR: "+JSON.parse(body).Year)
+        console.log("IMDB Rating: "+JSON.parse(body).imdbRating)
+        for (var i=0; i<JSON.parse(body).Ratings.length; i++)
+        {  
+            //console.log("entro al for")
+            if (JSON.parse(body).Ratings[i].Source === "Rotten Tomatoes"){
+            console.log("ROTTEN TOMATOES: "+JSON.parse(body).Ratings[i].Value)
+            temp = 1;
+         } }
+        if (temp === 0, console.log("ROTTEN TOMATOES: No aviable data"));
+        console.log("COUNTRY: "+JSON.parse(body).Country)
+        console.log("LANGUAGE: "+JSON.parse(body).Language)
+        console.log("PLOT: "+JSON.parse(body).Plot)
+        console.log("ACTORS: "+JSON.parse(body).Actors)
+        console.log("========= MOVIE THIS =========") 
+        //console.log(JSON.parse(body));
+        }
+        else{
+            console.log("========= MOVIE THIS =========") 
+            console.log("MOVIE NOT FOUND")
+            console.log("========= MOVIE THIS =========") 
+        }
+  }
+});
+}
+
 
 switch (command) {
-    case my-tweets:
+    case "my-tweets":
         tweets();
         break;
-    case spotify-this-song:
-       spotify();
+    case "spotify-this-song":
+        spotifythis();
         break;
-    case movie-this:
+    case "movie-this":
         movie();
         break;
-    case what-it-says:
+    case "do-what-it-says":
         says();
     break;
     default:
@@ -22,21 +134,3 @@ switch (command) {
 
 }
 
-var tweets = function(){
-    console.log("entro a twitter");
-    var Twitter = require('twitter');
- 
-var client = new Twitter({
-  consumer_key: keys.twitter.consumer_key,
-  consumer_secret: keys.twitter.consumer_secret,
-  access_token_key: keys.twitter.access_token_key,
-  access_token_secret: keys.twitter.access_token_secret
-});
-
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
-}
